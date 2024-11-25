3cc910b0b7ee47419bbd0399ea6c44518274ddc6
alininha24 <alineerodriguess27@gmail.com>
2024-11-25 09:14:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Física',
+    'Qual a diferença entre matéria escura e antimatéria??',
+    'A matéria escura, apesar dos estudos conduzidos, continua um mistério, a antimatéria consegue ser aprisionada, mas não completamente domada.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais país tem mais ilha no mundo?',
+    'Indonésia'
+)
+
+criaCartao(
+    'Programação',
+    'oque sgnifica a palavra determinaçao?',
+    ' Ato ou efeito de determinar(-se). 2 Resolução a que se chegou sobre algum assunto; decisão, deliberação. 3 Ato de avaliar, de estimar; cálculo, cômputo, contagem.'
+    
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'Amor em ingles é HI (love)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi tudo bem Inglês?',
+    'Oi em ingles é HI (hey hows it going)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz lindo em Inglês?',
+    'Oi em ingles é HI (beautiful)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz apaixonado em Inglês?',
+    'Oi em ingles é HI (passionate)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz estudante em Inglês?',
+    'Oi em ingles é HI (student)'
+)
